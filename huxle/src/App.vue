b579512a94fa19a88d07cebe362e4cc72efb9366
Kräutl Philip <philip.kraeutl@gmail.com>
2022-12-28 11:56:30 +0100

    implemented dynamic routing

--- huxle/src/App.vue
+++ huxle/src/App.vue
@@ -4,15 +4,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
---
+++ huxle/src/views/GameView.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import GameControl from "@/components/GameControl.vue";
+import LanguageComponent from "@/components/LanguageComponent.vue";
+import type { LanguageType } from "@/components/LanguageType";
+
+export default defineComponent({
+  components: { LanguageComponent, GameControl },
+  data() {
+    return {
+      selectedLanguage: "EN" as LanguageType,
+    };
+  },
+  methods: {
+    handleLanguageChange(language: LanguageType) {
+      this.selectedLanguage = language;
+    },
+  },
+  mounted() {
+    let id = this.$route.params.id;
+    console.log('The id is: ' + id);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <LanguageComponent @language-selected="handleLanguageChange" />
+    <GameControl solution="books" v-if="selectedLanguage === 'EN'" />
+    <GameControl solution="buche" v-else-if="selectedLanguage === 'DE'" />
+  </div>
+</template>
+
+<style></style>
