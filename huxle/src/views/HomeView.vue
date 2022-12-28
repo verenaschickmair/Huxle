7a005b7ee3fd0575c93767a8d2e110d878959756
Kräutl Philip <philip.kraeutl@gmail.com>
2022-12-28 11:40:27 +0100

    implemented language support

--- huxle/src/views/HomeView.vue
+++ huxle/src/views/HomeView.vue
@@ -1,16 +1,30 @@
 <script lang="ts">
+import { defineComponent } from "vue";
 import GameControl from "@/components/GameControl.vue";
+import LanguageComponent from "@/components/LanguageComponent.vue";
+import type { LanguageType } from "@/components/LanguageType";
 
-export default {
-  components: {GameControl}
-}
+export default defineComponent({
+  components: { LanguageComponent, GameControl },
+  data() {
+    return {
+      selectedLanguage: "EN" as LanguageType,
+    };
+  },
+  methods: {
+    handleLanguageChange(language: LanguageType) {
+      this.selectedLanguage = language;
+    },
+  },
+});
 </script>
 
 <template>
- <div>
-<GameControl solution="books"/>
- </div>
+  <div>
+    <LanguageComponent @language-selected="handleLanguageChange" />
+    <GameControl solution="books" v-if="selectedLanguage === 'EN'" />
+    <GameControl solution="buche" v-else-if="selectedLanguage === 'DE'" />
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
