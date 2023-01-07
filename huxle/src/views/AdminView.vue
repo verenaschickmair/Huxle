32d2254e9b45f25fdc6f1354f07d27097500f6f5
Kräutl Philip <philip.kraeutl@gmail.com>
2023-01-07 16:53:32 +0100

    added API Key supporter

--- huxle/src/views/AdminView.vue
+++ huxle/src/views/AdminView.vue
@@ -21,6 +21,7 @@ import type { WordData } from "@/types/WordData";
 import axios from "axios";
 import { defineComponent } from "vue";
 import { uuid } from "vue-uuid";
+import {ApiKey} from "@/api/ApiConfig";
 
 export default defineComponent({
   // TODO create input fields and fill wordGerman and wordEnglish
@@ -70,7 +71,7 @@ export default defineComponent({
           {
             headers: {
               Authorization:
-                "Bearer 24629ab19d2c880888663211a69c74783bf8e690e52783fbf69392b398245060254889e613c11c1be811bf88106d775ac0ecc72e763b7a6e478bae335099f3300b88dd72c77c38be3826342aaca5b5ddaf45450bc4ee1d252145b4190bb80771aa83579f54e2b8af4b7e9af55a76f10412c2a5d281195f43fcbebec139f50182",
+                "Bearer " + ApiKey
             },
           }
         )
--- huxle/src/views/GameView.vue
+++ huxle/src/views/GameView.vue
@@ -2,17 +2,18 @@
 import GameControl from "@/components/GameControl.vue";
 import LanguageComponent from "@/components/LanguageComponent.vue";
 import ModalComponent from "@/components/ModalComponent.vue";
-import type { LanguageType } from "@/types/LanguageType";
-import type { WordData } from "@/types/WordData";
+import type {LanguageType} from "@/types/LanguageType";
+import type {WordData} from "@/types/WordData";
 import axios from "axios";
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
+import {ApiKey} from "@/api/ApiConfig";
 
 export default defineComponent({
-  components: { LanguageComponent, GameControl, ModalComponent },
+  components: {LanguageComponent, GameControl, ModalComponent},
   data() {
     return {
       selectedLanguage: "DE" as LanguageType,
-      word: { wordId: "", wordGerman: "", wordEnglish: "" } as WordData,
+      word: {wordId: "", wordGerman: "", wordEnglish: ""} as WordData,
       showModal: false,
     };
   },
@@ -25,15 +26,14 @@ export default defineComponent({
     let id = this.$route.params.id;
     console.log("The id is: " + id);
 
-    const { data } = await axios.get(
-      `http://localhost:1337/api/words?filters[word_id]=${id}`,
-      {
-        headers: {
-          Authorization:
-            "Bearer " +
-              "b04ddd8b0e05dfb50ff7b0fb9a2f024f8eae719b45bcc5e0bad8245f8b02f2b45236093e513f3371f980028f8597c309d182ac091c29120d3b5c46b2be5b48f559f1d4bb1110e45067840f2347bf8140d6b32c954b115ebcbb4cd0d3c45ee37e9da8a787462d5d2858c5bf7ce5b87715c7c6604b533b13a0b8644baef35d7f2c",
-        },
-      }
+    const {data} = await axios.get(
+        `http://localhost:1337/api/words?filters[word_id]=${id}`,
+        {
+          headers: {
+            Authorization:
+                "Bearer " + ApiKey,
+          },
+        }
     );
 
     if (data.data.length) {
@@ -51,21 +51,21 @@ export default defineComponent({
 
 <template>
   <div>
-    <LanguageComponent @language-selected="handleLanguageChange($event)" />
+    <LanguageComponent @language-selected="handleLanguageChange($event)"/>
     <GameControl
-      :solution="word.wordEnglish"
-      v-if="selectedLanguage === 'EN'"
+        :solution="word.wordEnglish"
+        v-if="selectedLanguage === 'EN'"
     />
     <GameControl
-      :solution="word.wordGerman"
-      v-else-if="selectedLanguage === 'DE'"
+        :solution="word.wordGerman"
+        v-else-if="selectedLanguage === 'DE'"
     />
     <ModalComponent
-      :open="true"
-      headline="Oops..."
-      description="Sorry, the given link is invalid."
-      v-if="showModal"
-      @close="showModal = false"
+        :open="true"
+        headline="Oops..."
+        description="Sorry, the given link is invalid."
+        v-if="showModal"
+        @close="showModal = false"
     />
   </div>
 </template>
