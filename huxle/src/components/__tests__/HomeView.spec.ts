1404dbc9d2f364d803d91959d07c69038d4e8ac8
donmatzos <mgirkinger95@gmail.com>
2023-01-09 16:44:38 +0100

    refactoring

--- huxle/src/components/__tests__/HomeView.spec.ts
+++ huxle/src/components/__tests__/HomeView.spec.ts
@@ -1,19 +1,19 @@
-import {describe, it, expect} from "vitest";
-import {mount} from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
 import HomeView from "@/views/HomeView.vue";
 
 describe("HomeView", () => {
-    const wrapper = mount(HomeView)
-    it('should render correctly', () => {
-        expect(wrapper.html()).toMatchSnapshot()
-    });
-    it('should find admin button', () => {
-        expect(wrapper.find('[id="redirectToAdminButton"]').exists()).toBe(true)
-    });
-    it('should find game code button', () => {
-        expect(wrapper.find('[id="checkGameCodeButton"]').exists()).toBe(true)
-    });
-    it('should find game code input', () => {
-        expect(wrapper.find('[id="code"]').exists()).toBe(true)
-    });
-})
+  const wrapper = mount(HomeView);
+  it("should render correctly", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it("should find admin button", () => {
+    expect(wrapper.find('[id="redirectToAdminButton"]').exists()).toBe(true);
+  });
+  it("should find game code button", () => {
+    expect(wrapper.find('[id="checkGameCodeButton"]').exists()).toBe(true);
+  });
+  it("should find game code input", () => {
+    expect(wrapper.find('[id="code"]').exists()).toBe(true);
+  });
+});
