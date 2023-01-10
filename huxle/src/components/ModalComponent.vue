68966b70399dbbf833b6f28a7de6c8e23d92aa4b
Verena Schickmair <verena.schickmair@netural.com>
2023-01-10 13:56:18 +0100

    error handling input fields and responsive changes

--- huxle/src/components/ModalComponent.vue
+++ huxle/src/components/ModalComponent.vue
@@ -76,7 +76,7 @@ export default defineComponent({
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div
-          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+          class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
         >
           <TransitionChild
             as="template"
@@ -88,7 +88,7 @@ export default defineComponent({
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <DialogPanel
-              class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 w-full max-w-sm p-6"
             >
               <div>
                 <div class="mt-3 text-center sm:mt-5">
--- huxle/src/views/AdminView.vue
+++ huxle/src/views/AdminView.vue
@@ -1,136 +1,144 @@
 <template>
-  <div
-    class="relative h-screen bg-gray-400 lg:col-span-10 lg:col-start-3 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl"
-  >
+  <div class="relative h-screen bg-white py-16">
     <div
-      class="absolute inset-0 hidden overflow-hidden rounded-3xl lg:block"
-      aria-hidden="true"
-    >
-      <svg
-        class="absolute bottom-full left-full translate-y-1/3 -translate-x-2/3 transform xl:bottom-auto xl:top-0 xl:translate-y-0"
-        width="404"
-        height="384"
-        fill="none"
-        viewBox="0 0 404 384"
-        aria-hidden="true"
-      >
-        <defs>
-          <pattern
-            id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="4"
-              height="4"
-              class="text-gray-500"
-              fill="currentColor"
-            />
-          </pattern>
-        </defs>
-        <rect
-          width="404"
-          height="384"
-          fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
-        />
-      </svg>
-      <svg
-        class="absolute top-full -translate-y-1/3 -translate-x-1/3 transform xl:-translate-y-1/2"
-        width="404"
-        height="384"
-        fill="none"
-        viewBox="0 0 404 384"
-        aria-hidden="true"
-      >
-        <defs>
-          <pattern
-            id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="4"
-              height="4"
-              class="text-gray-500"
-              fill="currentColor"
-            />
-          </pattern>
-        </defs>
-        <rect
-          width="404"
-          height="384"
-          fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
-        />
-      </svg>
-    </div>
-    <div
-      class="relative flex lg:block justify-center space-y-12 py-12 px-4 sm:py-16 sm:px-6 lg:col-span-6 lg:col-start-4 lg:p-0"
+      class="relative h-full max-w-7xl mx-auto bg-gray-400 lg:col-span-10 lg:col-start-3 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl"
     >
       <div
-        class="relative space-y-12 py-12 px-4 sm:py-16 sm:px-6 lg:col-span-6 lg:col-start-4 lg:p-0"
+        class="absolute inset-0 hidden overflow-hidden rounded-3xl lg:block"
+        aria-hidden="true"
       >
-        <div class="space-y-3">
-          <div>
-            <h2
-              class="text-3xl my-3 font-bold tracking-tight text-white"
-              id="join-heading"
+        <svg
+          class="absolute bottom-full left-full translate-y-1/3 -translate-x-2/3 transform xl:bottom-auto xl:top-0 xl:translate-y-0"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
             >
-              Create your own Huxle
-            </h2>
-          </div>
-          <div>
-            <p
-              class="text-xl font-bold tracking-tight text-white"
-              id="join-heading"
-            >
-              German Word
-            </p>
-            <div class="mt-1">
-              <input
-                :class="{ 'border-red-500': this.errorGerman }"
-                @click="this.errorGerman = false"
-                type="text"
-                id="germanInput"
-                v-model="word.wordGerman"
-                class="w-full py-3 block sm:w-80 rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-500"
+                fill="currentColor"
               />
-            </div>
-          </div>
-          <div>
-            <p
-              class="text-xl font-bold tracking-tight text-white"
-              id="join-heading"
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="384"
+            fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+          />
+        </svg>
+        <svg
+          class="absolute top-full -translate-y-1/3 -translate-x-1/3 transform xl:-translate-y-1/2"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
             >
-              English Word
-            </p>
-            <div class="mt-1">
-              <input
-                :class="{ 'border-red-500': this.errorEnglish }"
-                @click="this.errorEnglish = false"
-                type="text"
-                id="englishInput"
-                v-model="word.wordEnglish"
-                class="w-full py-3 block sm:w-80 rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-500"
+                fill="currentColor"
               />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="384"
+            fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+          />
+        </svg>
+      </div>
+      <div
+        class="relative flex lg:block justify-center items-center h-full lg:h-auto space-y-12 py-12 px-4 sm:py-16 sm:px-6 lg:col-span-6 lg:col-start-4 lg:p-0"
+      >
+        <div
+          class="relative space-y-12 py-12 px-4 sm:py-16 sm:px-6 lg:col-span-6 lg:col-start-4 lg:p-0"
+        >
+          <div class="space-y-3">
+            <div>
+              <h2
+                class="text-3xl my-3 font-bold tracking-tight text-white"
+                id="join-heading"
+              >
+                Create your own Huxle
+              </h2>
             </div>
+            <div>
+              <p
+                class="text-xl font-bold tracking-tight text-white"
+                id="join-heading"
+              >
+                German Word
+              </p>
+              <div class="mt-1">
+                <input
+                  :class="{ 'border-red-500': errorGerman }"
+                  @click="errorGerman = false"
+                  type="text"
+                  id="germanInput"
+                  v-model="word.wordGerman"
+                  class="w-full py-3 block sm:w-80 rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+                />
+                <span v-if="errorGerman" class="text-red-500"
+                  >Please enter exactly 5 letters.</span
+                >
+              </div>
+            </div>
+            <div>
+              <p
+                class="text-xl font-bold tracking-tight text-white"
+                id="join-heading"
+              >
+                English Word
+              </p>
+              <div class="mt-1">
+                <input
+                  :class="{ 'border-red-500': errorEnglish }"
+                  @click="errorEnglish = false"
+                  type="text"
+                  id="englishInput"
+                  v-model="word.wordEnglish"
+                  class="w-full py-3 block sm:w-80 rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+                />
+              </div>
+              <span v-if="errorEnglish" class="text-red-500"
+                >Please enter exactly 5 letters.</span
+              >
+            </div>
+            <button
+              id="submitWordButton"
+              class="block w-full sm:w-80 rounded-md border border-transparent bg-gray-800 py-3 px-5 text-center text-base font-medium text-white shadow-md hover:bg-gray-600 sm:inline-block"
+              @click="upload()"
+            >
+              Generate Huxle
+            </button>
           </div>
-          <button
-            id="submitWordButton"
-            class="block w-full rounded-md border border-transparent bg-gray-800 py-3 px-5 text-center text-base font-medium text-white shadow-md hover:bg-gray-600 sm:inline-block sm:w-auto"
-            @click="upload()"
-          >
-            Generate Huxle
-          </button>
         </div>
       </div>
     </div>
@@ -179,15 +187,17 @@ export default defineComponent({
       this.word.wordGerman = this.word.wordGerman.toLowerCase();
     },
     checkInputs(): boolean {
+      console.log(this.word.wordEnglish);
+      console.log(this.word.wordGerman);
       if (this.word.wordEnglish.length !== 5) {
         this.errorEnglish = true;
-        return false;
       }
+
       if (this.word.wordGerman.length !== 5) {
         this.errorGerman = true;
-        return false;
       }
-      return true;
+
+      return !this.errorEnglish && !this.errorGerman ? true : false;
     },
     async upload() {
       if (!this.checkInputs()) {
